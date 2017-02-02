98313c161ce6039e8a663f8436c344e60c3a9e33
Marcin Nowicki <pr0d1r2@gmail.com>
2017-02-02 07:41:09 +0100

    Allow to force explicit descriptions in rspec. Useful for doctest

---
+++ git-hooks/pre_commit/ensure_explicit_descriptions_in_rspec.rb
@@ -0,0 +1,31 @@
+module Overcommit::Hook::PreCommit
+  class EnsureExplicitDescriptionsInRspec < Base
+    BLOCK_NAMES = %i[describe context].freeze
+
+    def run
+      errors = detect_errors
+      return :fail, errors.join("\n") unless errors.empty?
+
+      :pass
+    end
+
+    private
+
+    def detect_errors
+      check_files.map do |file|
+        file_contents = File.read(file)
+        BLOCK_NAMES.map do |block_name|
+          if file_contents.include?("#{block_name} do")
+            ["#{file}: contains '#{block_name} do' (please use explicit description like: #{block_name} 'this block does' do)"]
+          end
+        end
+      end.flatten.compact
+    end
+
+    def check_files
+      applicable_files.reject do |file|
+        File.basename(file) =~ /^ensure_explicit_descriptions_in_rspec\.rb$/
+      end
+    end
+  end
+end
