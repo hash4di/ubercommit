6d65e5f2962ebe109fac9d51b8c84f4b8cad70d6
Marcin Nowicki <pr0d1r2@gmail.com>
2017-10-04 07:30:39 +0200

    Only warn about possible contain_exactly

--- git-hooks/pre_commit/ensure_better_array_matching_in_rspec.rb
+++ git-hooks/pre_commit/ensure_better_array_matching_in_rspec.rb
@@ -7,33 +7,20 @@
 module Overcommit::Hook::PreCommit
   class EnsureBetterArrayMatchingInRspec < Base
     def run
-      errors = detect_errors
-      if errors.empty?
-        warnings = detect_warnings
-        return :warn, warnings.join("\n") unless warnings.empty?
-      else
-        return :fail, errors.join("\n")
-      end
-
+      warnings = detect_warnings
+      return :warn, warnings.join("\n") unless warnings.empty?
       :pass
     end
 
     private
-
-    def detect_errors
-      check_files.map do |file|
-        file_contents = File.read(file)
-        if file_contents =~ / eq\(\[(.{1,})\]\)/
-          "#{file}: contains ' eq([#{$1}])' (can be replaced by ' contain_exactly(#{$1})')`"
-        end
-      end.compact
-    end
 
     def detect_warnings
       check_files.map do |file|
         file_contents = File.read(file)
         if file_contents.include?(' eq([])')
           "#{file}: contains ' eq([])' (can be replaced by ' be_empty')`"
+        elsif file_contents =~ / eq\(\[(.{1,})\]\)/
+          "#{file}: contains ' eq([#{$1}])' (can be replaced by ' contain_exactly(#{$1})')`"
         end
       end.compact
     end
