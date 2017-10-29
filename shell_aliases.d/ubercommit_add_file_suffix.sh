af2e362a9aaff76f846937ed1c3c202042d1036d
Marcin Nowicki <pr0d1r2@gmail.com>
2017-10-29 06:17:57 +0100

    Do not add ruby checks only for overcommit hooks

--- shell_aliases.d/ubercommit_add_file_suffix.sh
+++ shell_aliases.d/ubercommit_add_file_suffix.sh
@@ -1,6 +1,6 @@
 function ubercommit_add_file_suffix() {
-  # shellcheck disable=SC2046
-  if [ $(find . -type f -name "*.$1" | wc -l) -gt 0 ]; then
+  # shellcheck disable=SC2046,SC2126
+  if [ $(find . -type f -name "*.$1" | grep -v "^./.git-hooks/" | wc -l) -gt 0 ]; then
     find "$UBERCOMMIT_PATH/git-hooks" -type f -name "*.rb" | \
       parallel "grep -q \"^#     - '\\*\\*/\\*\\.$1'\" {} && echo {} | sed -e 's|$UBERCOMMIT_PATH/||'" | \
         parallel "test -d .{//} || mkdir -p .{//} ; cp $UBERCOMMIT_PATH/{} .{}" || return $?
