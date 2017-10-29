f95063533c158249faf0298d4e5c76e3bb960928
Marcin Nowicki <pr0d1r2@gmail.com>
2017-10-29 06:44:12 +0100

    Remove variable

--- shell_aliases.d/ubercommit_add.sh
+++ shell_aliases.d/ubercommit_add.sh
@@ -1,15 +1,13 @@
 function ubercommit_add() {
-  local ubercommit_add_COMMAND_DEPENDENCIES
-  ubercommit_add_COMMAND_DEPENDENCIES="source $UBERCOMMIT_PATH/shell_aliases.d/UBERCOMMIT_PATH.sh"
-  ubercommit_add_COMMAND_DEPENDENCIES+=" && source $UBERCOMMIT_PATH/shell_aliases.d/ubercommit_add_file_suffix.sh"
-  ubercommit_add_COMMAND_DEPENDENCIES+=" && source $UBERCOMMIT_PATH/shell_aliases.d/ubercommit_add_file_suffix_examples.sh"
-
   if [ -z "$SKIP_EXAMPLES" ]; then
     echo 'PreCommit:' > .overcommit.yml.example.ubercommit
   fi
 
   parallel \
-    "$ubercommit_add_COMMAND_DEPENDENCIES && SKIP_EXAMPLES='$SKIP_EXAMPLES' ubercommit_add_file_suffix {}" \
+    "source $UBERCOMMIT_PATH/shell_aliases.d/UBERCOMMIT_PATH.sh && \
+     source $UBERCOMMIT_PATH/shell_aliases.d/ubercommit_add_file_suffix.sh && \
+     source $UBERCOMMIT_PATH/shell_aliases.d/ubercommit_add_file_suffix_examples.sh && \
+     SKIP_EXAMPLES='$SKIP_EXAMPLES' ubercommit_add_file_suffix {}" \
     ::: \
     sh rb && \
       overcommit --sign pre-commit
