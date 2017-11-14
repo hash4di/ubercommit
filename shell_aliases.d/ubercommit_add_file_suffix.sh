81ba64a7142bbf961f37396e30081d71e0110ad6
Marcin Nowicki <pr0d1r2@gmail.com>
2017-11-14 04:53:18 +0100

    Support subdirectories for file suffixes

--- shell_aliases.d/ubercommit_add_file_suffix.sh
+++ shell_aliases.d/ubercommit_add_file_suffix.sh
@@ -2,7 +2,7 @@ function ubercommit_add_file_suffix() {
   # shellcheck disable=SC2046,SC2126
   if [ $(find . -type f -name "*.$1" | grep -v "^./.git-hooks/" | wc -l) -gt 0 ]; then
     find "$UBERCOMMIT_PATH/git-hooks" -type f -name "*.rb" | \
-      parallel "grep -q \"^#     - '\\*\\*/\\*\\.$1'\" {} && echo {} | sed -e 's|$UBERCOMMIT_PATH/||'" | \
+      parallel "grep -q \"^#     - '.*/\\*\\.$1'\" {} && echo {} | sed -e 's|$UBERCOMMIT_PATH/||'" | \
         parallel "test -d .{//} || mkdir -p .{//} ; cp $UBERCOMMIT_PATH/{} .{}" || return $?
     if [ -z "$SKIP_EXAMPLES" ]; then
       ubercommit_add_file_suffix_examples "$1" || return $?
