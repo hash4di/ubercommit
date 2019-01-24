a338fa32c993203068a694e10ae828c682e01afc
Marcin Nowicki <pr0d1r2@gmail.com>
2019-01-24 09:55:50 +0000

    Warn about using '.exists?' that sometimes can be replaced with '.any?'

---
+++ git-hooks/pre_commit/warn_about_using_exists.rb
@@ -0,0 +1,29 @@
+# Example configuration:
+#
+# WarnAboutUsingExists:
+#   enabled: true
+#   include:
+#     - '**/*.rb'
+module Overcommit
+  module Hook
+    module PreCommit
+      # Warns about '.exists?' which in some cases can be replaced with '.any?' for speed
+      class WarnAboutUsingExists < Base
+        def run
+          return :pass if warnings.empty?
+          [:warn, warnings.join("\n")]
+        end
+
+        private
+
+        def warnings
+          @warnings ||= applicable_files.map do |file|
+            if File.read(file).include?('.exists?')
+              "#{file}: contains '.exists?', you may want to check if '.any?' can be used"
+            end
+          end
+        end
+      end
+    end
+  end
+end
